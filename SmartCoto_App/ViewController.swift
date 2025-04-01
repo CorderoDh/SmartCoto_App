639b055178376082245f81110fb5eb0426f6bbf0
Carlos Cordero <corderillo@icloud.com>
2025-04-01 11:46:55 -0600

    Initial Commit

---
+++ SmartCoto_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCoto_App
+//
+//  Created by Carlos Miguel on 01/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
