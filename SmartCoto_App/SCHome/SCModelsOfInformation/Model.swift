4002ec08cd775b31a3c125ebc81e612d473da10f
Carlos Cordero <corderillo@icloud.com>
2025-04-09 19:45:09 -0600

    Guardando cambios

--- SmartCoto_App/SCHome/SCModelsOfInformation/Model.swift
+++ SmartCoto_App/SCHome/SCModelsOfInformation/Model.swift
@@ -9,16 +9,44 @@
 import SwiftUI
 import Combine
 
-class Model: ObservableObject {
-    @Published var title: String
-    @Published var quantity: String
-    @Published var porcent: String
-    @Published var date: String
-    
-    init(title: String, quantity: String, porcent: String, date: String) {
-        self.title = title
-        self.quantity = quantity
-        self.porcent = porcent
-        self.date = date
-    }
+struct Model {
+    var title: String
+    var quantity: Int
+    var porcent: String
+    var date: String
+}
+
+struct Today {
+    var access: Model = Model(title: "Total de accesos", quantity: 16, porcent: "+10%", date: "de hoy")
+    var reception: Model = Model(title: "Paquetes en recepción", quantity: 32, porcent: "-5%", date: "de hoy")
+    var guest: Model = Model(title: "Invitados", quantity: 4, porcent: "+32%", date: "de hoy")
+    var events: Model = Model(title: "Eventos", quantity: 3, porcent: "-3%", date: "de hoy")
+}
+
+struct Yesterday {
+    var access: Model = Model(title: "Total de accesos", quantity: 16, porcent: "+10%", date: "de ayer")
+    var reception: Model = Model(title: "Paquetes en recepción", quantity: 32, porcent: "-5%", date: "de ayer")
+    var guest: Model = Model(title: "Invitados", quantity: 4, porcent: "+32%", date: "de ayer")
+    var events: Model = Model(title: "Eventos", quantity: 3, porcent: "-3%", date: "de ayer")
+}
+
+struct ThisWeek {
+    var access: Model = Model(title: "Total de accesos", quantity: 16, porcent: "+10%", date: "de la semana")
+    var reception: Model = Model(title: "Paquetes en recepción", quantity: 32, porcent: "-5%", date: "de la semana")
+    var guest: Model = Model(title: "Invitados", quantity: 4, porcent: "+32%", date: "de la semana")
+    var events: Model = Model(title: "Eventos", quantity: 3, porcent: "-3%", date: "de la semana")
+}
+
+struct ThisMont {
+    var access: Model = Model(title: "Total de accesos", quantity: 16, porcent: "+10%", date: "del mes")
+    var reception: Model = Model(title: "Paquetes en recepción", quantity: 32, porcent: "-5%", date: "del mes")
+    var guest: Model = Model(title: "Invitados", quantity: 4, porcent: "+32%", date: "del mes")
+    var events: Model = Model(title: "Eventos", quantity: 3, porcent: "-3%", date: "del mes")
+}
+
+struct ThisYear {
+    var access: Model = Model(title: "Total de accesos", quantity: 16, porcent: "+10%", date: "del año")
+    var reception: Model = Model(title: "Paquetes en recepción", quantity: 32, porcent: "-5%", date: "del año")
+    var guest: Model = Model(title: "Invitados", quantity: 4, porcent: "+32%", date: "del año")
+    var events: Model = Model(title: "Eventos", quantity: 3, porcent: "-3%", date: "del año")
 }
